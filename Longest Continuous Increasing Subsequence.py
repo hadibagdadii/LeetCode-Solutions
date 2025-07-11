379933d9465b903370176a358304d1bbf0373045
Hadi Bagdadi <104606024+hadibagdadii@users.noreply.github.com>
2025-07-11 16:57:06 -0700

    Create Longest Continuous Increasing Subsequence.py

---
+++ Longest Continuous Increasing Subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        longest, current = 1, 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                current += 1
+            else:
+                current = 1
+            longest = max(longest, current)
+        return longest
