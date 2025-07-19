b6d7ae591f3d49689b33ba38d11df62be6e4e1d6
Hadi Bagdadi <104606024+hadibagdadii@users.noreply.github.com>
2025-07-19 12:35:35 -0700

    Create Squares of a Sorted Array.py

---
+++ Squares of a Sorted Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i, n in enumerate(nums):
+            nums[i] = n ** 2
+        nums.sort()
+        return nums
