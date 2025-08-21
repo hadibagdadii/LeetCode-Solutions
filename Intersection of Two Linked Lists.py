f48939e64641d320a0b18179ac89eb3457508d29
Hadi Bagdadi <104606024+hadibagdadii@users.noreply.github.com>
2025-08-21 15:34:33 -0700

    Create Intersection of Two Linked Lists.py

---
+++ Intersection of Two Linked Lists.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None
+        
+        pointerA = headA
+        pointerB = headB
+        
+        while pointerA != pointerB:
+            pointerA = pointerA.next if pointerA else headB
+            pointerB = pointerB.next if pointerB else headA
+        
+        return pointerA
